32393e55b57f1bfe0c3ce637259414f679ec254c
sial <rimsila.itc@gmail.com>
2021-11-02 22:09:38 +0700

    added cicd to surge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const IndexPage = () => {
     <Layout pageTitle="Home | Next.js + TypeScript Example">
       <div className="max-w-lg mx-auto mt-20">
         <h1 className="text-3xl font-extrabold text-black dark:text-white sm:text-4xl text-center mb-6 mt-6 leading-10">
-          {t('example.helloUser', { name: ': Sila' })}
+          {t('example.helloUser', { name: ': Sila Love' })}
         </h1>
         <h2 className="font-extrabold text-black dark:text-white text-3xl text-center mb-6 mt-6">
           Testing GraphQl Api with {CREDENTIAL.GRAPHQL_API_ENDPOINT}
