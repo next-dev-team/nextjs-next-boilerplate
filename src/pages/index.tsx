c16460dd58c891b3cd1d4a6ee6f1cd2ae4fedff6
Sila Rim <51617383+rimsila@users.noreply.github.com>
2021-11-22 17:45:34 +0700

    update example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,34 @@ import { Layout } from '@/components/module';
 import { CREDENTIAL } from '@/constants';
 import { usePostQuery } from '@/graphQl/hooks';
 import { t } from '@/locales';
-import { Pricing2 } from 'components-next';
+import { useReactive } from 'ahooks';
+import { omit } from 'lodash';
 import React from 'react';
 
 const IndexPage = () => {
+  const state = useReactive({
+    variables: {
+      id: 1,
+    },
+  });
+
   /**
    *
    * get post using graphQl
    */
-  const { data } = usePostQuery({
-    variables: { id: '1' },
+  const { data, loading } = usePostQuery({
+    variables: { ...(state.variables as any) },
+  });
+
+  console.log('data', data);
+
+  const renderObj = Object.keys(omit(data?.post, '__typename')).map((i, k) => {
+    const objVal = Object.values(omit(data?.post, '__typename'))[k];
+    return (
+      <p key={i}>
+        <span className="font-bold">{i}</span>:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{objVal}
+      </p>
+    );
   });
 
   return (
@@ -23,12 +41,15 @@ const IndexPage = () => {
         <h2 className="font-extrabold text-black dark:text-white text-3xl text-center mb-6 mt-6">
           Testing GraphQl Api with {CREDENTIAL.GRAPHQL_API_ENDPOINT}
         </h2>
-      </div>
-
-      <div className="flex justify-center mt-10">
-        <div className="max-w-md">
-          <Pricing2 />
-        </div>
+        {loading ? <p>Loading...</p> : renderObj}
+        <button
+          className="bg-green-400 p-2 mt-4 rounded-md"
+          onClick={() => {
+            state.variables.id += 1;
+          }}
+        >
+          page {state.variables.id}
+        </button>
       </div>
     </Layout>
   );
