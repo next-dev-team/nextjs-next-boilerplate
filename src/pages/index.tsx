51c29390ac00fd39853cf89ed9e847a220e63266
sial <rimsila.itc@gmail.com>
2022-04-24 13:23:36 +0700

    test deep level dynamic route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,11 @@ const IndexPageTemplate = dynamic(() => import('@/components/template/home'));
 
 const IndexPage = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example" className="px-20 py-6">
+    <Layout
+      description="this is my homepage description"
+      title="Home | Next.js + TypeScript Example"
+      className="px-20 py-6"
+    >
       <IndexPageTemplate />
     </Layout>
   );
